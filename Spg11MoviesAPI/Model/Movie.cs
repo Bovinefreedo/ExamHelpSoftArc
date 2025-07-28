60b900748f6640c37e54b92a3127276935b4d3f0
BovineFreedo <frankskorstengaard@gmail.com>
2025-07-28 15:08:12 +0200

    Made movies api

---
+++ Spg11MoviesAPI/Model/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace Spg11MoviesAPI.Model
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string title { get; set; } = "No title";
+        public int year { get; set; }
+        public List<string> genres { get; set; } = new();
+
+    }
+}
---
+++ Spg11MoviesAPI/Program.cs
@@ -0,0 +1,51 @@
+using Spg11MoviesAPI.Model;
+using System.Reflection.Metadata.Ecma335;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+List<Movie> movies = new();
+
+app.MapGet("/api/movies", () => movies);
+app.MapGet("/api/movies/{id}", (int id) => movies.FirstOrDefault(m => id == m.id));
+app.MapPost("/api/movies", (MovieData newMovie) =>{
+    Movie m = new Movie
+    {
+        id = movies[movies.Count - 1].id + 1,
+        title = newMovie.title,
+        year = newMovie.year,
+        genres = newMovie.genres
+    };
+    movies.Add(m);
+    return Results.Ok();
+});
+app.MapPut("/api/movies/{id}", (int id, MovieData updatedData) => {
+    Movie? m = movies.FirstOrDefault(x => x.id == id);
+    if (m == null)
+    {
+        return Results.BadRequest();
+    }
+    else {
+        m.title = updatedData.title;
+        m.year = updatedData.year;
+        m.genres = updatedData.genres;
+        return Results.Ok();
+    }
+});
+app.MapDelete("/api/movies/{id}", (int id) => movies.RemoveAll(m => m.id == id));
+app.MapGet("/api/movies/genre/{genre}", (string genre) => {
+    List<Movie> m = movies.Where(m => m.genres.Contains(genre)).ToList();
+    return Results.Ok(m);
+});
+
+
+app.Run();
+
+public record MovieData(string title, int year, List<string> genres);
